0025714b837396a75c6db2e1e2606509909029f5
Agnes Cameron <agnesfcameron@gmail.com>
2023-06-17 16:14:22 +0100

    drawing circles

--- webmidi-test.js
+++ webmidi-test.js
@@ -7,26 +7,43 @@ WebMidi
   .catch(err => alert(err));
 
 
-function dec2bin(dec) {
-  return (dec >>> 0).toString(2);
-}
+var params = {
+  fullscreen: true
+};
+
+
+var elem = document.body;
+var two = new Two(params).appendTo(elem);
 
 
 function drawLine(data) {
   console.log(data)
+
+  var radius = data[1];
+  var x = two.width * 0.5;
+  var y = two.height * 0.5 - radius * 1.25;
+  var circle = two.makeCircle(x, y, radius);
+
+  // The object returned has many stylable properties:
+  circle.fill = '#FF8000';
+  // And accepts all valid CSS color:
+  circle.stroke = 'white';
+  circle.linewidth = 5;
+
+  two.update();
 }
 
 // Function triggered when WEBMIDI.js is ready
 function onEnabled() {
 
   // Display available MIDI input devices
-  if (WebMidi.inputs.length < 1) {
-    document.body.innerHTML+= "No device detected.";
-  } else {
-    WebMidi.inputs.forEach((device, index) => {
-      document.body.innerHTML+= `${index}: ${device.name} <br>`;
-    });
-  }
+  // if (WebMidi.inputs.length < 1) {
+  //   document.body.innerHTML+= "No device detected.";
+  // } else {
+  //   WebMidi.inputs.forEach((device, index) => {
+  //     document.body.innerHTML+= `${index}: ${device.name} <br>`;
+  //   });
+  // }
 
   const roli = WebMidi.inputs[0]
   // console.log(roli.eventNames)
@@ -53,6 +70,14 @@ function onEnabled() {
   //   document.body.innerHTML+= `${e.note.name} <br>`;
   // });
 
+
+  roli.addListener("onnote", e => {
+    // console.log(e.rawData)
+    // 0 is status, 1 is data 1, 2 is data 2
+    // console.log(dec2bin(e.rawData[0]))
+    drawLine(e.rawData)
+  })
+
     roli.addListener("pitchbend", e => {
     // console.log(e.rawData)
     // 0 is status, 1 is data 1, 2 is data 2
@@ -61,3 +86,8 @@ function onEnabled() {
   });
 
 }
+
+function dec2bin(dec) {
+  return (dec >>> 0).toString(2);
+}
+
